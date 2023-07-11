9d7bba55922c3d2dbe15221b3963de549733f840
volirra <127318805+volirra@users.noreply.github.com>
2023-07-11 21:13:56 +0300

    commit-4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
+const items = []
+
+galleryItems.forEach(element => {
+	const galleryItem = document.createElement('div')
+	galleryItem.className = 'gallery__item'
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.alt = element.description;
+
+	galleryItem.append(galleryLink)
+	galleryLink.append(galleryImage)
+	items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" >
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionData: 'alt',
+	captionDelay: 250
+})
